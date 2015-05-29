fb556d33c29b7c7f201f4b60b26444b80e347662
brycehammond <hammond.bryce99@gmail.com>
2015-05-29 19:34:41 -0400

    added subscribe button

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   root 'posts#index'
    get 'static/index' => 'static#index'
 
-  # The priority is based upon order of creation: first created -> highest priority.
+  # The priority isß based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
