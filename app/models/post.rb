43b9d51cbf8a26efae3e3e9fbfc459497d7d19fa
brycehammond <hammond.bryce99@gmail.com>
2015-06-29 09:24:41 -0400

    config chages for heroku

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,17 @@
 class Post < ActiveRecord::Base
 	has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#"},
-	              :storage => :s3,
-		 		  :s3_credentials => "#{Rails.root}/config/S3.yml",
-                  :path => ":attachment/:id/:style/:filename.:extension",
-                  :bucket => ENV['S3_BUCKET_NAME']
+	:storage => :s3,
+	:s3_credentials => Proc.new{|a| a.instance.s3_credentials }
 	validates_attachment_content_type :image, content_type:  /\Aimage\/.*\Z/
 	has_many :comments, dependent: :destroy
 	validates :title, presence: true, length: { minimum: 5 }
 	validates :body, presence: true
 	is_impressionable 
+
+	def s3_credentials
+		{:bucket => ENV["S3_BUCKET_NAME"], :access_key_id => ENV["AWS_ACCESS_KEY_ID"], 
+        :secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"]
+    end
 
 	after_save :send_posts
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
   # config.action_controller.asset_host = "http://localhost:3000"
 
   config.paperclip_defaults = {
-  :storage => :S3,
+  :storage => :s3,
   :s3_credentials => {
     :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
